194f3763ba81af9ccadc98a62306d17d4bbeb8ce
charlesbooth <bharlescooth@gmail.com>
2023-02-12 21:43:19 -0600

    Stripped alg_test file

--- test/alg_test.py
+++ test/alg_test.py
@@ -1,13 +1,6 @@
-from bisect import bisect_left
-
 
 def main():
-    fin = open('words.txt')
-    form = [word.rstrip().lower()
-            for word in fin]
-    while True:
-        i = bisect_left(form, 'a', key=lambda x: x[0])
-        print(i, form[i])
+    pass
 
 
 if __name__ == "__main__":
